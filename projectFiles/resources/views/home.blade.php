16054ea7dfc4ff4d4e8e39d083295c095e4bb97d
Ethan <etmi7372@colorado.edu>
2024-06-21 15:03:15 -0600

    partials structure and initial familiarization work.

---
+++ projectFiles/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@include('partials.header')
+<div class="container">
+		<br>
+		<br>
+		<div class="row">
+			<div class="col-8">
+				<div class="jumbotron">
+				  <h1 class="display-4">Sample Title</h1>
+				  
+				</div>
+			</div>
+		</div>
+	</div>
