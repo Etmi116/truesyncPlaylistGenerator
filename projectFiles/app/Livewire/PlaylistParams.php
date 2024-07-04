ec6598558d2647c9912d616d78564386802416ee
Ethan <etmi7372@colorado.edu>
2024-07-04 11:55:31 -0600

    livewire elements

--- projectFiles/app/Livewire/PlaylistParams.php
+++ projectFiles/app/Livewire/PlaylistParams.php
@@ -2,12 +2,14 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class PlaylistParams extends Component
 {   
     //playlist gen variables
-    public $title;
+    #[Validate('required|min:5',onUpdate:false,message:'Please provide a title for your playlist')]
+    public $title ='';
 
     public $ProgrammaticTime;
 
@@ -15,30 +17,67 @@ class PlaylistParams extends Component
 
     public $VenueSpecificTime;
 
+    public $result = '';
+
 
     //building the table
+    #[Validate('required',onUpdate:false,message:'You need at least one genre')]
     public $genres = [];
 
     public $select ='';
     
- 
-    public function add()
+    
+    public function build(){
+        $validate = $this->validate();
+        $this->result = $validate;
+    }
+
+    //function to add genre type to the above array from the dropdown menu
+    public function addGenre()
     {
         if(in_array($this->select,$this->genres) or $this->select == ''){
             return;
         }
+
         $this->genres[] = $this->select;
 
         $this->select ='';
     }
 
-    public function remove($option){
+    //function called when the delete key is pressed in the genre table
+    //does not currently adjust time variable.
+    public function removeGenre($option){
         $key = array_search($option,$this->genres);
         array_splice($this->genres,$key,1);
     }
- 
-    public function render()
-    {
-        return view('livewire.playlist-params');
+
+    //gives venue specific a space between text.
+    //simple and could be improved upon but fast.
+    public function displayGenre($label){
+        if($label == 'VenueSpecific'){return 'Venue Specific';}
+
+        return $label;
     }
+    
+    //debug function
+    public function timeHelper($label){
+        switch($label){
+            default:
+            case 'Programmatic':
+                return $this->ProgrammaticTime;
+            case 'Entertainment':
+                return $this->EntertainmentTime;
+            case 'VenueSpecific':
+                return $this->VenueSpecificTime;
+            
+        }
+    }
+
+
+      //render function
+      public function render()
+      {
+          return view('livewire.playlist-params');
+      }
+  
 }
--- projectFiles/resources/views/livewire/playlist-params.blade.php
+++ projectFiles/resources/views/livewire/playlist-params.blade.php
@@ -1,10 +1,13 @@
 <!--livewire bits -->
 <div class="bodybox">
     <p>Edit a previously created dynamic playlist to the system so that you can assign it to a player.</p>
+    <h3>Playlist Name</h3>
+    <input type='text' wire:model.blur="title" class = "wide-text-input" placeholder="Title"></input>
+    <div class ="errorMessage">@error('title') {{$message}} @enderror</div>
     <h3>Standard Content</h3>
     <h3>Content Type</h3>
     <!-- Form to add content type to a playlist -->
-    <form class="row" wire:submit="add">
+    <form class="row" wire:submit="addGenre">
         
         <div class="column">
             <select class="dropdown" wire:model="select" name="type" id="test">
@@ -18,16 +21,29 @@
             <button class='blueButton' type="submit">+Add</button>
         </div>
     </form>
+    <!--displays title to show dynamic updating on clientside -->
+    <h4>{{$title}}</h4>
     <!-- Loops through small database to display chosen content types -->
-    @foreach ($genres as $select)
+    @foreach ($genres as $option)
         <div class="row-border">
-            <div class="column">
-                <p>{{$select}}</p>
+            <div class="columnB">
+                <p>
+                    {{ $this->displayGenre($option) }}
+                </p>
+                <p>
+                    {{ $this->timeHelper($option) }}
+                </p>
+            </div>
+            <div class="columnB">
+                <input type="number" wire:model.blur="{{$option}}Time" placeholder="Duration"></input>
             </div>
             <div class = "column">
-                <button wire:click="remove('{{$select}}')" class="blueButton">Delete</button>
+                <button wire:click="removeGenre('{{$option}}')" class="redButton">Remove</button>
             </div>
         </div>
     @endforeach
-    
+    <div class ="errorMessage">@error('genres') {{$message}} @enderror</div>
+    <form wire:submit="build">
+        <button class="blueButton" type="submit">Build My Playlist</button>
+    </form>
 </div>
